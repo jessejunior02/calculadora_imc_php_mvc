7708c1d7a8f84e3e93dac22a3eb4424f5c9d2ee0
jessejunior02 <jessepcarvalhojunior02@gmail.com>
2024-08-20 01:01:01 -0300

    :sparkles: feat: Criação do arquivo Controller

--- app/Controllers/IMCController.php
+++ app/Controllers/IMCController.php
@@ -1,40 +1,54 @@
 <?php
 
-namespace App\Controllers;
+require __DIR__ . '/../vendor/autoload.php';
 
-use App\Models\IMCDados;
-use App\Models\IMCCalculo;
 use App\Models\IMCAnalise;
+use App\Models\IMCCalculo;
+use App\Models\IMCDados;
 
 /**
- * Controller responsável por lidar com a lógica de cálculo e análise de IMC.
+ * Controlador responsável pelo fluxo de dados entre o Model e a View.
  * 
  * @author Jesse Junior <jessepcarvalhojunior02@gmail.com>
  */
 
-Class IMCController
-{
-    public function handleRequest()
-    {
-        // Verificando o método de request
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            $altura = $_POST['altura'] ?? 0;
-            $peso = $_POST['peso'] ?? 0;
+ 
+/**
+ * Armazenamento de classes dentro de variáveis
+ * 
+ */
+$imcDados = new IMCDados();
+$imcCalculo = new IMCCalculo($imcDados);
+$imcAnalise = new IMCAnalise($imcDados);
 
-            // Instanciar o modelo de dados e definir os valores recebidos
-            $dadosIMC = new IMCDados();
-            $dadosIMC->setAltura($altura);
-            $dadosIMC->setPeso($peso);
 
-            // Calcular o IMC
-            $calculoIMC = new IMCCalculo($dadosIMC);
-            $calculoIMC->calcularIMC();
+/**
+ * Verificando se determiados POST condiz com o formulário de entrada
+ */
+if(isset($_POST['altura']) && isset($_POST['peso'])){
+    $altura = $_POST['altura'];
+    $peso = $POST['peso'];
 
-            // Analisar o resultado do IMC
-            $analiseIMC = new IMCAnalise($dadosIMC);
-            $resultado = $analiseIMC->analiseResultado();
-            // Exibir o resultado
-            /* Retornar View  */
-        }
-    }
+
+    /**
+     * Passar os valores coletados para as classes Models.
+     */
+    // Atualizando o valo do metodo setAltura() para ser utilizado no getAltura()
+    $imcDados->setAltura($altura);
+    $imcDados->setPeso($peso);
+    
+    // Retornando o calculo
+    $imcCalculo->calcularIMC();
+    
+    // Armazenando o IMC final em uma variável
+    $imcCalculado = $imcDados->getImc();
+    
+    // Retornando a Analise do IMC
+    $resultadoAnalise = $imcAnalise->analiseResultado();
+
+    // Retornando os dados para a view
+    include __DIR__ . '/../Views/app_view.php';
+} else {
+    // Caso não haja POST, mostrar o formulário
+    include __DIR__ . '/../Views/app_view.php';
 }
