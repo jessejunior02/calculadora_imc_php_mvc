aaa88d55eee30779d071110d77246803b452656c
jessejunior02 <jessepcarvalhojunior02@gmail.com>
2024-08-20 00:59:36 -0300

    :sparkles: feat: Criação do arquivo index do Public

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Este é o ponto de entrada principal da aplicação, que redireciona para o controlador principal
+ * 
+ * @author Jesse Junior <jessepcarvalhojunior02@gmail.com>
+ */
+
+require __DIR__ . '/../app/Controllers/IMCController.php';
