4abaf2e8f7de32e74ed754432648270f562f0a7e
jessejunior02 <jessepcarvalhojunior02@gmail.com>
2024-08-20 02:04:21 -0300

    :sparkles: feat: Criação do arquivo Controlle

---
+++ app/Views/error_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erro 404</title>
+</head>
+<body>
+    <h1>404 - Página Não Encontrada</h1>
+    <p>Desculpe, a página que você está procurando não existe.</p>
+    <a href="/">Voltar para a página inicial</a>
+</body>
+</html>
